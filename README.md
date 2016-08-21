bef151f42b3df45869d608bffc3b829c0dd1d726
Jason Duffy <jasonsduffy@gmail.com>
2016-08-21 16:24:12 -0400

    test for struct parameters

--- README.md
+++ README.md
@@ -4,3 +4,58 @@ A mocking library for Swift
 
 Stay tuned... this is a work in progress.
 
+## Stubbing
+
+First, make a mock object with a mutable instance of MockingBird.
+
+Then in the function you want to stub you can use `valueFor` to get the value to return.
+
+In your test, you can use `when` to set the stubbed value.
+
+```swift
+import MockingBird
+
+struct MockStruct {
+    var mb = MockingBird()
+
+    func foo() -> String {
+        return mb.valueFor("foo") ?? ""
+    }
+}
+
+class StructTests: XCTestCase {
+
+    let mock = MockStruct()
+
+    func testItWorks() {
+        mock.mb.when("foo", thenReturn: "bar")
+
+        XCTAssertEqual("bar", mock.foo())
+    }
+}
+
+```
+You can also stub for specific set of arguments:
+
+```swift
+import MockingBird
+
+struct MockStruct {
+    var mb = MockingBird()
+
+    func validate(id: Int, name: String) -> Bool {
+        return mb.valueFor("validate", [id, name]) ?? false
+    }
+}
+
+class StructTests: XCTestCase {
+
+    let mock = MockStruct()
+
+    func testItWorks() {
+        mock.mb.when("validate", [27, "George"], thenReturn: true)
+
+        XCTAssertTrue(mock.validate(27, "George"))
+    }
+}
+```
--- Tests/MockingBirdTests/MockingBirdTests.swift
+++ Tests/MockingBirdTests/MockingBirdTests.swift
@@ -19,11 +19,28 @@ class MockingBirdTests: XCTestCase {
         XCTAssertEqual("One", mock.say(1))
         XCTAssertEqual("Two", mock.say(2))
     }
+
+    func testConditionalStubbingWithStructParams() {
+        mock.mb.when("getNumber", [TestStruct(num: 1, name: "Hi")], thenReturn: 5)
+        mock.mb.when("getNumber", [TestStruct(num: 3, name: "Yo")], thenReturn: 2)
+        let thirdStruct = TestStruct(num: 7, name: "Third")
+        mock.mb.when("getNumber", [thirdStruct], thenReturn: 9)
+
+        XCTAssertEqual(5, mock.getNumber(TestStruct(num: 1, name: "Hi")))
+        XCTAssertEqual(2, mock.getNumber(TestStruct(num: 3, name: "Yo")))
+        XCTAssertEqual(9, mock.getNumber(thirdStruct))
+    }
+}
+
+struct TestStruct {
+    let num: Int
+    let name: String
 }
 
 protocol CanBeMocked {
     func greet() -> String
     func say(_ number: Int) -> String
+    func getNumber(_ s: TestStruct) -> Int
 }
 
 struct MockGreeter: CanBeMocked {
@@ -35,5 +52,9 @@ struct MockGreeter: CanBeMocked {
 
     func say(_ number: Int) -> String {
         return mb.valueFor("say", [number]) ?? ""
+    }
+
+    func getNumber(_ s: TestStruct) -> Int {
+        return mb.valueFor("getNumber", [s]) ?? 0
     }
 }
